249141e8e2a18261d14eaaaebfe77228f7eecd98
karl-police <karl-police2001@bluewin.ch>
2025-01-19 18:00:45 +0100

    Initial commit

---
+++ src/MainApp.cpp
@@ -0,0 +1,17 @@
+﻿// MainApp.cpp : Defines the entry point for the application.
+//
+
+#include "MainApp.hpp"
+#include <QApplication>
+#include <QMainWindow>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    QMainWindow w;
+    w.show();
+
+    return a.exec();
+}
